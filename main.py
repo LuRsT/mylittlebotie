e33c06ddb65a0329175459062badb43e2b129a2f
Gil Gonçalves <lursty@gmail.com>
2017-07-11 15:36:19 +0100

    Simplification intensifies :no_mouth:

--- main.py
+++ main.py
@@ -4,18 +4,12 @@ import random
 from birdy.twitter import UserClient
 
 
-CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
-CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
-ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
-
-
 def send_twitt(status):
     client = UserClient(
-        CONSUMER_KEY,
-        CONSUMER_SECRET,
-        ACCESS_TOKEN,
-        ACCESS_TOKEN_SECRET,
+        os.environ['TWITTER_CONSUMER_KEY'],
+        os.environ['TWITTER_CONSUMER_SECRET'],
+        os.environ['TWITTER_ACCESS_TOKEN'],
+        os.environ['TWITTER_ACCESS_TOKEN_SECRET'],
     )
     client.api.statuses.update.post(status=status)
     print("You just sent a twitt! Good job!")
