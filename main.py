a87bba3afa47c739e4bf534d5a8650a8c20d3900
Gil Gonçalves <lursty@gmail.com>
2017-07-11 15:07:22 +0100

    Simplified the whole thing

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import os
 import random
 
-from client import CLIENT
+from birdy.twitter import UserClient
+
+
+CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+
+
+def send_twitt(status):
+    client = UserClient(
+        CONSUMER_KEY,
+        CONSUMER_SECRET,
+        ACCESS_TOKEN,
+        ACCESS_TOKEN_SECRET,
+    )
+    client.api.statuses.update.post(status=status)
+    print("You just sent a twitt! Good job!")
 
 
 if __name__ == '__main__':
     die_result = random.randint(1,6)
     status = 'I just threw a die and the result was {}'.format(die_result)
-    response = CLIENT.api.statuses.update.post(status=status)
+    send_twitt(status)
